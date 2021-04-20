354546eac40668f3a6b3ae70dc93f53dbd89ab13
dudejark <dudejark@gmail.com>
2021-04-20 13:03:43 +0530

    Add files via upload

---
+++ xls_to_xlsx.py
@@ -0,0 +1,4 @@
+import pyexcel as p
+
+p.save_book_as(file_name='/Users/amandudeja/Desktop/Python39/D__ExportData.xls',
+               dest_file_name='/Users/amandudeja/Desktop/Python39/D__ExportData.xlsx')
