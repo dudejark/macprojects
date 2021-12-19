ae9c413b3717839df405ec32bcca963fc854180c
Ram Dudeja <dudejark@gmail.com>
2021-12-19 16:56:19 +0530

    Revert "added comments"
    This reverts commit 2ac7ce185026426a22b73afb1db3829dc3514b4b.

--- func1.py
+++ func1.py
@@ -2,8 +2,12 @@ def sum(num1, num2):
     sum1 = num1 + num2
     return sum1
     
-#this is where the function values get printed
+
 print(sum(4, 5))
 
+<<<<<<< HEAD
  #this is where the function values get printed
   #this is where the function values get printed 3rd comment
+=======
+ 
+>>>>>>> parent of 2ac7ce1 (added comments)
