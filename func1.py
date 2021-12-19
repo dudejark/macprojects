2ac7ce185026426a22b73afb1db3829dc3514b4b
Ram Dudeja <dudejark@gmail.com>
2021-12-19 16:34:59 +0530

    added comments

--- func1.py
+++ func1.py
@@ -2,7 +2,7 @@ def sum(num1, num2):
     sum1 = num1 + num2
     return sum1
     
-
+#this is where the function values get printed
 print(sum(4, 5))
 
- 
+ #this is where the function values get printed
